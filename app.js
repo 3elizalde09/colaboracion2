e256d03848a8edf1ad877b2a207aabb86f5b85f8
3elizalde09 <je.elizalde09@gmail.com>
2024-11-29 17:06:26 -0600

    mi primer archivo y cambio

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log("Hello I am JS, externo");
+alert("este es un ejemplo de una alerta, no es recomendado porque se considera invasion al usuario");
+let nameUser = window.prompt("Ingrese su nombre");
